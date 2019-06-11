9081c9570fcb8a9b9c2cf78952c02d099a1bfced
Giftsonj <50142321+Giftsonj@users.noreply.github.com>
2019-06-11 17:26:57 +0530

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { SupportMainComponent } from './support-main/support-main.component';
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponentComponent,
     SupportMainComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- support-main.component.ts
+++ support-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-support-main',
@@ -9,7 +10,71 @@ import { ActivatedRoute } from '@angular/router'
 export class SupportMainComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
   user: string = this.route.snapshot.paramMap.get('id');
+  // isBcColor: boolean = true;
+  letStyles: {
+    // "background-color": "green",
+  };
+  isClicked = false;
+  // letStyles = {
+  //   "background-color": "skyblue",
+  // }
+  mainProd(evt) {
+    // alert()
+    // console.log("hii how are you")
+    // this.letStyles = {
+    //   "background-color": "green",
 
+    // }
+    var buttons = document.getElementsByClassName("btnStyle");
+    // console.log(evt)
+    // buttons.evt.target
+    // *[NgFor]
+    for (var i = 0; i < buttons.length; i++) {
+
+      // if (!(buttons[i] == evt.target)) {
+      //   this.letStyles = {
+      //     "background-color": "white",
+
+      //     // $scope.myObj = {
+      //     //   "color" : "white",
+      //     //   "background-color" : "coral",
+      //     //   "font-size" : "60px",
+      //     //   "padding" : "50px"
+      //     // }
+      //   }
+      // }
+      if (buttons[i] == evt.target) {
+        console.log(buttons[i].className)
+
+        evt.target.id = true;
+        buttons[i].classList.add("activeclass");
+
+        // this.letStyles = {
+        //   "background-color": "skyblue",
+        // evt.target.sty
+        // $scope.myObj = {
+        //   "color" : "white",
+        //   "background-color" : "coral",
+        //   "font-size" : "60px",
+        //   "padding" : "50px"
+        // }
+        // }
+
+        // buttons[i]
+      }else{
+      buttons[i].classList.remove("activeclass");
+      // buttons[i]. = "white";
+      // console.log(buttons[i])
+      // var varWhiteColor = buttons[i].
+      // console.log("dsdsd"+evt.target.id)
+      // this.s
+    }
+  }
+
+    // var isClicked = false;
+    // return this.letStyles;
+
+  }
   ngOnInit() {
   }
 
